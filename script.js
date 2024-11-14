8848a0044e295293b9f11892e8ebab5306116292
Your Name <you@example.com>
2024-11-13 19:36:22 -0800

    added requirements for the first one

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ myGrades = [100, 100, 90, 73, 78, 94, 86];
 
 myAverage = 0;
 
-for (let i = 1; i <= myGrades.length; i++) {
+for (let i = 0; i < myGrades.length; i++) {
 
 myAverage += myGrades[i];
 
@@ -11,3 +11,5 @@ myAverage += myGrades[i];
 myAverage = myAverage / myGrades.length;
 
 console.log("My grade average is " + myAverage);
+
+// the 1 in (let i = 1) should've been a 0. there shouldn't be a = in the <=. all i did to fix it is i just replaced the 1 with a 0 and deleted the =
