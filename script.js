a018eae8ac5063183884032aaa1af73187d4afae
Your Name <you@example.com>
2024-11-13 22:37:23 -0800

    added second requirement

--- script.js
+++ script.js
@@ -12,4 +12,19 @@ myAverage = myAverage / myGrades.length;
 
 console.log("My grade average is " + myAverage);
 
-// the 1 in (let i = 1) should've been a 0. there shouldn't be a = in the <=. all i did to fix it is i just replaced the 1 with a 0 and deleted the =
+// the 1 in (let i = 1) should've been a 0. there shouldn't be a = in the <=. all i did to fix it is i just replaced the 1 with a 0 and deleted the =.
+
+function checkNumber(value) {
+    if (isNaN(value)) {
+        throw new Error("Input must be a number.");
+    }
+    if (value < 0) {
+        throw new Error("Input must not be less than 0.");
+    }
+    if (value > 100) {
+        throw new Error("Input must not be greater than 100.");
+    }
+    return `Valid input: ${value}`;
+}
+
+
